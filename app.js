453f76c14960393282b70f1c13605b77103f201a
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-14 22:59:07 +0530

    Feat: Transalator JS file added

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Welcome JS is working in HTML");
+var inputText = document.querySelector("#input-text");
+
+var translateBtn = document.querySelector("#translate-btn");
+
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+    return serverURL+"?text="+input
+}
+
+function errorHandler(error) {
+    console.log("Error Occured", error);
+    alert("Something wrong with server !, try after some time.")
+}
+
+function clickHandller() {
+    fetch (getTranslationURL(inputText.value))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)    
+};
+
+translateBtn.addEventListener("click", clickHandller)
+
+// console.log("Welcome JS is working in HTML");
